09b3275d2000a1cd48836fec01cc81eba23f6775
devoil <devdevoildev@gmail.com>
2025-02-23 23:20:01 +0900

    ✨ Feat: 비디오 카드 만들기 / timeago.js 추가

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,5 +1,24 @@
-export const VideoCard = ({ video }) => {
-  return <div>{video.snippet.title}</div>;
+import { formatAgo } from '../util/date';
+import Youtube from '../api/youtube';
+
+type VideoType = Awaited<ReturnType<Youtube['fetchVideos']>>[0];
+
+interface VideoCardProps {
+  video: VideoType;
+}
+
+export const VideoCard = ({ video }: VideoCardProps) => {
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  return (
+    <li>
+      <img src={thumbnails.medium.url} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p>{channelTitle}</p>
+        <p>{formatAgo(publishedAt, 'ko')}</p>
+      </div>
+    </li>
+  );
 };
 
 export default VideoCard;
